d7fd33625f693e2865e97dd81814a543ef1e02a7
Paweł Sypek <paw3lsyp3k@gmail.com>
2022-02-08 23:13:49 +0100

    adding about coop section

---
+++ js/app.js
@@ -0,0 +1,13 @@
+const items = document.querySelectorAll('.dropdown-item');
+const title = document.querySelector(".dropdown-title");
+
+
+
+items.forEach((e) => e.addEventListener("click", () => {
+    if(e.classList.contains("active")){
+        e.classList.remove("active")
+    } else{
+        items.forEach((e) => e.classList.remove("active"));
+        e.classList.add("active");
+    }   
+}));
